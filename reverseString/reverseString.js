bbff4b534a7f272438d5d833bcd906a1be3add1a
Harprit Singh <Singhhs@users.noreply.github.com>
2020-04-26 16:44:44 +0200

    finshed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
-}
+const reverseString = function(str) {
+    let newReverseStr  = "";  
+    for (i = str.length - 1; i >= 0; i--){
+      newReverseStr += str[i] }
+      return newReverseStr
+  }
 
 module.exports = reverseString
