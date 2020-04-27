b1512d0e572256be54eac5611868b089cbe62ea8
Harprit Singh <Singhhs@users.noreply.github.com>
2020-04-27 23:01:04 +0200

    fixed leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
-}
-
+const leapYears = function(year) {
+    const centuryYear = year % 100 == 0 && year % 400 != 0;
+    const leapYear    = year % 4 == 0;
+  
+    if (centuryYear || !leapYear) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 module.exports = leapYears
+
+
+// Leap years are years divisible by four (like 1984 and 2004)
+// years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000
+// use an `if` statement and `&&` to make sure all the conditions are met properly
