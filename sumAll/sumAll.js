8b55720a0d2073e528308d03963fb36e783472ba
Harprit Singh <Singhhs@users.noreply.github.com>
2020-04-27 15:04:20 +0200

    Finished sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let sum = 0
+    
+    if(typeof firstInt == typeof secondInt){
+             if(firstInt > secondInt) {
+                 let temp = secondInt;
+                 secondInt = firstInt;
+                 firstInt = temp;
+             }
+             if(firstInt < 0 || secondInt < 0) return 'ERROR';
+                 for(let i = firstInt; i <= secondInt; ++i){
+                     sum += i;
+                 }
+         } else return 'ERROR';
+         
+         return sum;
+   
+   }
 
-}
 
 module.exports = sumAll
