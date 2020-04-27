3886f1200698f128eff202d98c3304f07d073bb5
Harprit Singh <Singhhs@users.noreply.github.com>
2020-04-27 23:33:08 +0200

    finished tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  let returnValue = (number - 32) * (5/9) //Formular  [°F] = [°C] × ​9⁄5 + 32
+  let rounded = Math.round(returnValue * 10) / 10 //rounded to one decimal place
+  return rounded
 }
 
-const ctof = function() {
-
+const ctof = function(number) {
+let returnValue = number * (9/5) + 32 // Fomular 	[°C] = ([°F] − 32) × ​5⁄9
+let rounded = Math.round(returnValue * 10) / 10 //rounded to one decimal place
+  return rounded
 }
 
 module.exports = {
